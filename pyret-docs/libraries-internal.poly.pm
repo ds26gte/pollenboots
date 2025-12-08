2ef431e0f8503968c8b53788873c10463bc5d767
Dorai Sitaram <ds26gte@gmail.com>
2025-12-08 09:39:01 -0500

    add builtin/raw-arrays.poly.pm

--- pyret-docs/libraries-internal.poly.pm
+++ pyret-docs/libraries-internal.poly.pm
@@ -1,5 +1,18 @@
 #lang pollen
 
-◊title{Internal Libraries}
+◊title[#:style '(toc)]{Internal Libraries}
 
-testing
+The following libraries are primarily meant for developers of Pyret.
+Their documentation may be poor or missing.
+You are unlikely to find much of interest here unless you're part of
+the developer team; if you are, ask us for help directly.
+
+◊(table-of-contents)
+
+◊include-section["builtin/raw-arrays.html"]
+◊; ◊include-section["trove/data-source.scrbl"]
+◊; ◊include-section["trove/srcloc.js.rkt"]
+◊; ◊include-section["trove/pprint.js.rkt"]
+◊; ◊include-section["trove/s-exp.js.rkt"]
+◊; ◊include-section["trove/s-exp-structs.js.rkt"]
+
