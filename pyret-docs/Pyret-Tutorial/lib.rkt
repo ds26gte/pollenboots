b4c186bb9a8581fbaeb0fe53badb872f43c94429
Dorai Sitaram <ds26gte@gmail.com>
2026-01-22 10:34:42 -0500

    - Remove all pyret-docs content - Restrict README.adoc to pollenboots

--- pyret-docs/Pyret-Tutorial/lib.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket/base
-
-(require "../common-tags.rkt")
-
-(provide [all-defined-out])
-
-;;;; <LOCAL ADDITIONS>
-
-(define (show-url u) (hyperlink u u))
--- pyret-docs/glossary.poly.pm
+++
@@ -1,5 +0,0 @@
-#lang pollen
-
-◊title{Glossary}
-
-◊(custom-index-block)
